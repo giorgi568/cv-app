61d94773d42014c2ca70cb0802985841ebc6d248
giorgi568 <mateshvili02@mail.ru>
2023-12-02 23:43:18 +0400

    basic info form ready

--- src/components/infoPanel.jsx
+++ src/components/infoPanel.jsx
@@ -1,10 +1,32 @@
 export default function InfoPanel() {
   return (
     <div className="infoPanel">
-      <form action="getInformation" className="form" id="form">
+    <form action="getInformation" className="form" id="form">
+
+    <fieldset>
+      <legend>Basic Info</legend>
+
+      <label htmlFor="firstName">First Name:</label>
+      <input type="text" id="firstName" required />
+      
+      <label htmlFor="lastName">Last Name:</label>
+      <input type="text" id="lastName" required />
 
 
-      </form>
+      <label htmlFor="email">Email:</label>
+      <input type="email" id="email" placeholder="example@mail.com" required />
+
+
+      <label htmlFor="phoneNumber">Phone Number:</label>
+      <input type="text" id="phoneNumber" required />
+
+      <label htmlFor="description">Write Something About You</label>
+      <textarea name="description" id="description" cols="30" rows="10"></textarea>
+    </fieldset>
+
+    <input type="submit" id="submit" value="Submit" />
+
+    </form>
     </div>
   )
 }
